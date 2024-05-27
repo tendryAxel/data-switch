53de183c78b16fb61e56b4b4e7e67f9ccb8afc2a
tendryAxel <hei.tendry@gmail.com>
2024-05-27 06:56:31 +0300

    feat: CLI python

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from src.main import MultiConversion
 
 if __name__ == '__main__':
-    c = MultiConversion("test")
+    print(f"{'-'*20}\nWelcome to this Python application\n{'-'*20}\n")
+    print("Todo:")
+    print("\t- Create the folder /in")
+    print("\t- put all of your .csv file in the folder in /in")
+    c = MultiConversion(input("The name of the folder in /in: "))
     c.convert()
-    c.save(result_type="psql")
+    c.save(result_type=input("The type of result(json/psql): "))
